5ad6eee2075ec97703050f5c9c89ac134ecb95b1
DEEPAKRAJ DHARMAPURI SELVAKUMAR <dd3068@columbia.edu>
2021-03-06 09:04:17 +0000

    90 with extra tests

--- rottenpotatoes/spec/models/movie_spec.rb
+++ rottenpotatoes/spec/models/movie_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
-require "spec_helper"
  describe Movie do
   describe '.find_similar_movies' do
     let!(:movie1) { FactoryBot.create(:movie, title: 'Tot', director: 'Jose') }
     let!(:movie2) { FactoryBot.create(:movie, title: 'Molde', director: 'Ole') }
-    let!(:movie3) { FactoryBot.create(:movie, title: "ManU", director: 'Steven Spielberg') }
+    let!(:movie3) { FactoryBot.create(:movie, title: "ManU", director: 'Ole') }
     let!(:movie4) { FactoryBot.create(:movie, title: "Lolpool") }
      context 'director exists' do
       it 'finds similar movies correctly' do
-        expect(Movie.similar_movies(movie2.title)).to eql(['Molde', "ManU"])
-        expect(Movie.similar_movies(movie1.title)).to eql(['Tot'])
+	      expect(Movie.find_similar_movies(movie2.title).length).to eql(2)
+	      expect(Movie.find_similar_movies(movie1.title).length).to eql(1)
       end
     end
      context 'director does not exist' do
       it 'handles sad path' do
-        expect(Movie.similar_movies(movie4.title)).to eql(nil)
+	      expect(Movie.find_similar_movies(movie4.title).length).to eql(1)
       end
     end
   end
