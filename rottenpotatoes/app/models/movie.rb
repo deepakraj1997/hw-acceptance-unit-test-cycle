af75afce417fb35fa12ab69e9ee608c70b27f188
DEEPAKRAJ DHARMAPURI SELVAKUMAR <dd3068@columbia.edu>
2021-03-06 05:29:35 +0000

    All pass

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
   def self.find_similar_movies movie_title
     director = Movie.find_by(title: movie_title).director
-    if director.nil? 
+    if director.nil? or director.empty? 
 	return nil 
     end
     Movie.where(director: director)
