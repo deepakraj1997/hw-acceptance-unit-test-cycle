510a551597e4ee522d8831fc93a8db4654d24b19
DEEPAKRAJ DHARMAPURI SELVAKUMAR <dd3068@columbia.edu>
2021-03-07 04:57:56 +0000

    79% coveraage

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -17,10 +17,13 @@ end
 
 describe MoviesController do
  describe 'search movies with same director' do
-     #it 'Call Movie.similar_movies' do
-     #  get :search, { title: 'Star Wars' }
-     #  expect(Movie).to receive(:find_similar_movies).with('Star Wars')
-     #end
+     it 'Call Movie.similar_movies' do
+      get :search, { title: 'Star Wars' }
+      res = []
+      Movie.find_similar_movies('Star Wars').each { |mov| res.append(mov.director) }
+      puts(Movie.find_similar_movies('Star Wars').class)
+      expect(res.to_set).to eq(["George Lucas"].to_set)
+     end
      it 'Call Movie.similar_movies 2' do
        get :search, { title: 'Star Wars' }
        expect(response).to have_http_status(:ok)
@@ -31,14 +34,36 @@ describe MoviesController do
       expect(response).to redirect_to(root_url)
      end
  end
-  describe 'GET new' do
+ describe 'GET new' do
     let!(:movie) { Movie.new }
      it 'should render the new template' do
       get :new
       expect(response).to render_template('new')
     end
-  end
-
+ end
+ describe 'Index Page' do
+    let!(:movie) {FactoryBot.create(:movie)}
+     it 'Home Page' do
+      get :index
+      expect(response).to render_template('index')
+    end
+ end
+ describe 'Show Page' do
+    let!(:movie) { FactoryBot.create(:movie) }
+    before(:each) do
+      get :show, id: movie.id
+    end
+     it 'Show Page' do
+      expect(response).to render_template('show')
+    end
+ end
+ describe 'After clicking Create' do
+    it 'Should be in Index Page' do
+     post :create, movie: FactoryBot.attributes_for(:movie)
+     expect(response).to redirect_to(movies_url)
+    end
+ end
+ 
 end
 
 
